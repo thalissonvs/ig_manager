37a213e0ddd337254152195bc4fe61e1c0f93096
Thalison <thalissfernandes99@gmail.com>
2024-03-31 16:42:18 -0300

    methods to update config widgets values created

--- src/gui/views/main_view.py
+++ src/gui/views/main_view.py
@@ -1,9 +1,48 @@
 from PyQt5.QtWidgets import QMainWindow
 
-from src.gui.views.main_view import GUIKwaiBot
+from src.gui.resources.main_view_rc import IGBotGUI
 
 
-class MainView(QMainWindow, GUIKwaiBot):
-    def __init__(self) -> None:
-        super(MainView, self).__init__()
+class MainView(IGBotGUI, QMainWindow):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
         self.setupUi(self)
+
+    def set_time_between_actions_min_value(self, value: int) -> None:
+        self.spinbox_min_time_actions.setValue(value)
+
+    def set_time_between_actions_max_value(self, value: int) -> None:
+        self.spinbox_max_time_actions.setValue(value)
+
+    def set_actions_to_switch_account_value(self, value: int) -> None:
+        self.spinbox_actions_change_amount.setValue(value)
+
+    def set_switch_account_with_no_tasks_value(self, value: bool) -> None:
+        self.radiobutton_change_actions.setChecked(value)
+        self.radiobutton_dont_change_actions.setChecked(not value)
+
+    def set_time_without_tasks_to_wait_value(self, value: int) -> None:
+        self.spinbox_seconds_to_change.setValue(value)
+
+    def set_perform_like_actions_value(self, value: bool) -> None:
+        self.checkbox_like.setChecked(value)
+
+    def set_perform_follow_actions_value(self, value: bool) -> None:
+        self.checkbox_follow.setChecked(value)
+
+    def set_enable_goal_value(self, value: bool) -> None:
+        self.radiobutton_enable_goal.setChecked(value)
+        self.radiobutton_disable_goal.setChecked(not value)
+
+    def set_actions_goal_value(self, value: int) -> None:
+        self.spinbox_goal_actions.setValue(value)
+
+    def set_enable_rest_goal_value(self, value: bool) -> None:
+        self.radiobutton_enable_rest_goal.setChecked(value)
+        self.radiobutton_disable_rest_goal.setChecked(not value)
+
+    def set_rest_goal_actions_value(self, value: int) -> None:
+        self.spinbox_actions_rest.setValue(value)
+
+    def set_rest_goal_time_value(self, value: int) -> None:
+        self.spinbox_minutes_rest.setValue(value)
