52330f222772d9e97e576bee40babfa201cf03d1
Thalison <thalissfernandes99@gmail.com>
2024-03-31 16:48:28 -0300

    `ConfigModel` signals connected to `MainView` methods

--- src/gui/views/main_view.py
+++ src/gui/views/main_view.py
@@ -4,9 +4,37 @@ from src.gui.resources.main_view_rc import IGBotGUI
 
 
 class MainView(IGBotGUI, QMainWindow):
-    def __init__(self, parent=None) -> None:
+    def __init__(self, config_model, parent=None) -> None:
         super().__init__(parent)
         self.setupUi(self)
+        self.config_model = config_model
+        self.connect_config_model_signals()
+
+    def connect_config_model_signals(self) -> None:
+        self.config_model.time_between_actions_min_changed.connect(
+            self.set_time_between_actions_min_value)
+        self.config_model.time_between_actions_max_changed.connect(
+            self.set_time_between_actions_max_value)
+        self.config_model.actions_to_switch_account_changed.connect(
+            self.set_actions_to_switch_account_value)
+        self.config_model.switch_account_with_no_tasks_changed.connect(
+            self.set_switch_account_with_no_tasks_value)
+        self.config_model.time_without_tasks_to_wait_changed.connect(
+            self.set_time_without_tasks_to_wait_value)
+        self.config_model.perform_like_actions_changed.connect(
+            self.set_perform_like_actions_value)
+        self.config_model.perform_follow_actions_changed.connect(
+            self.set_perform_follow_actions_value)
+        self.config_model.enable_goal_changed.connect(
+            self.set_enable_goal_value)
+        self.config_model.actions_goal_changed.connect(
+            self.set_actions_goal_value)
+        self.config_model.enable_rest_goal_changed.connect(
+            self.set_enable_rest_goal_value)
+        self.config_model.rest_goal_actions_changed.connect(
+            self.set_rest_goal_actions_value)
+        self.config_model.rest_goal_time_changed.connect(
+            self.set_rest_goal_time_value)
 
     def set_time_between_actions_min_value(self, value: int) -> None:
         self.spinbox_min_time_actions.setValue(value)
