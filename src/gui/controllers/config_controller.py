617041dd6e99f4ec52336ad3c5004ff630d6d1fd
Thalison <thalissfernandes99@gmail.com>
2024-04-06 19:58:37 -0300

    `ConfigController` refactored to pass tests

--- src/gui/controllers/config_controller.py
+++ src/gui/controllers/config_controller.py
@@ -1,27 +1,15 @@
 import json
 import os
 
-from PyQt5.QtCore import QObject
+from PyQt5.QtCore import QObject, pyqtSignal
 
+from src.gui.constants import DEFAULT_OPTIONS
 from src.gui.models.config_model import ConfigModel
 
 
 class ConfigController(QObject):
 
-    DEFAULT_OPTIONS = {
-        'time_between_actions_min': 5,
-        'time_between_actions_max': 10,
-        'actions_to_switch_account': 50,
-        'switch_account_with_no_tasks': True,
-        'time_without_tasks_to_wait': 30,
-        'perform_like_actions': True,
-        'perform_follow_actions': True,
-        'enable_goal': True,
-        'actions_goal': 200,
-        'enable_rest_goal': True,
-        'rest_goal_actions': 25,
-        'rest_goal_time': 60,
-    }
+    show_popup_signal = pyqtSignal(str, str)
 
     OPTIONS_PATH = os.path.join(os.getcwd(), 'userdata', 'options.json')
 
@@ -30,7 +18,7 @@ class ConfigController(QObject):
         self.config_model = config_model
 
     def set_default_options(self) -> None:
-        for key, value in self.DEFAULT_OPTIONS.items():
+        for key, value in DEFAULT_OPTIONS.items():
             setattr(self.config_model, key, value)
 
     def set_options_if_valid(self) -> None:
@@ -46,17 +34,33 @@ class ConfigController(QObject):
             self.set_default_options()
             return
 
+        if not set(options.keys()) == set(DEFAULT_OPTIONS.keys()):
+            self.set_default_options()
+            return
+
         self.load_options(options)
 
     def load_options(self, options: dict) -> None:
+
+        if not set(options.keys()) == set(DEFAULT_OPTIONS.keys()):
+            raise AttributeError('Invalid options keys')
+
         for key, value in options.items():
-            if key in self.DEFAULT_OPTIONS:
+            if key in DEFAULT_OPTIONS:
                 setattr(self.config_model, key, value)
 
     def save_options(self, options: dict) -> None:
+
+        if not set(options.keys()) == set(DEFAULT_OPTIONS.keys()):
+            raise AttributeError('Invalid options keys')
+
         for key, value in options.items():
-            if key in self.DEFAULT_OPTIONS:
+            if key in DEFAULT_OPTIONS:
                 setattr(self.config_model, key, value)
 
         with open(self.OPTIONS_PATH, 'w') as f:
-            f.write(json.dumps(options, ident=4))
+            f.write(json.dumps(options, indent=4))
+
+        self.show_popup_signal.emit(
+            'Sucesso!', 'Configurações salvas com sucesso!'
+        )
