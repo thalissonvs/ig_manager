1eb50c2ec3f97257f15661ec2213b2e98e654fd9
Thalison <thalissfernandes99@gmail.com>
2024-04-30 02:32:49 -0300

    class `ProfilesService` created to format profiles text from gui to a dict

---
+++ src/gui/services/profiles_service.py
@@ -0,0 +1,14 @@
+class ProfilesService:
+
+    @staticmethod
+    def format_profiles_text(profiles_text: str) -> list[dict]:
+        profiles = []
+        for profile in profiles_text.split('\n'):
+            if profile:
+                username, password, gender = profile.split(' ')
+                profiles.append({
+                    'username': username,
+                    'password': password,
+                    'gender': gender
+                })
+        return profiles
