71b0f96117cbfd66d81e0e142a330fa8c4cd08c7
Thalison <thalissfernandes99@gmail.com>
2024-05-03 05:00:26 -0300

    concrete classes for `ISelectors` created

---
+++ src/selectors/official_instagram_selectors.py
@@ -0,0 +1,19 @@
+from src.interfaces.i_selectors import ISelectors
+
+
+class OfficialInstagramSelectors(ISelectors):
+    @property
+    def LOGIN_BUTTON(self) -> dict:
+        return {'resourceId': 'com.instagram.android:id/log_in_button'}
+
+    @property
+    def USERNAME_INPUT(self) -> dict:
+        return {'resourceId': 'com.instagram.android:id/login_username'}
+
+    @property
+    def PASSWORD_INPUT(self) -> dict:
+        return {'resourceId': 'com.instagram.android:id/password'}
+
+    @property
+    def SUBMIT_BUTTON(self) -> dict:
+        return {'resourceId': 'com.instagram.android:id/next_button'}
