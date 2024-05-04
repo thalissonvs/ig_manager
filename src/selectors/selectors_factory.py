7724fbf4913637b5f8e048853bba5bdc04b25f36
Thalison <thalissfernandes99@gmail.com>
2024-05-04 17:59:47 -0300

    created classes with instagram selectors and a factory

--- src/selectors/selectors_factory.py
+++ src/selectors/selectors_factory.py
@@ -1,20 +1,17 @@
 from src.interfaces.i_selectors import ISelectors
-from src.selectors.lite_instagram_selectors import LiteInstagramSelectors
-from src.selectors.official_instagram_selectors import (
+from src.selectors.lite_instagram_app import LiteInstagramSelectors
+from src.selectors.official_instagram_app import (
     OfficialInstagramSelectors,
 )
 
 
 class SelectorsFactory:
     def create_selectors(
-        self, automation_platform: str, automation_app: str
+        self, automation_app: str
     ) -> ISelectors:
-        if automation_platform == 'android':
-            if automation_app == 'official_instagram':
-                return OfficialInstagramSelectors()
-            elif automation_app == 'lite_instagram':
-                return LiteInstagramSelectors()
-            else:
-                raise ValueError('Invalid automation app')
+        if automation_app == 'official_instagram':
+            return OfficialInstagramSelectors()
+        elif automation_app == 'lite_instagram':
+            return LiteInstagramSelectors()
         else:
-            raise ValueError('Invalid automation platform')
+            raise ValueError('Invalid automation app')
