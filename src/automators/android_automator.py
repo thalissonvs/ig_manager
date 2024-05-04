2bb7e05ee035586d83e6afd15c70aa856cae276c
Thalison <thalissfernandes99@gmail.com>
2024-05-04 18:00:28 -0300

    some small changes in `AndroidAutomator`

--- src/automators/android_automator.py
+++ src/automators/android_automator.py
@@ -1,25 +1,44 @@
 import uiautomator2 as u2
 
-from src.interfaces.i_automator import IAutomator
 
-
-class AndroidAutomator(IAutomator):
-    def __init__(self, device_info: dict) -> None:
+class AndroidAutomator:
+    def __init__(self, device_info: dict, automation_app: str) -> None:
         self.device_info = device_info
+        self.automation_app = automation_app
         self.device_automator = u2.connect(self.device_info['device_id'])
 
-    def click(self, element_selectors: dict, timeout=10) -> None:
+    def prepare(self) -> int:
+        packages_map = {
+            'official_instagram': 'com.instagram.android',
+            'lite_instagram': 'com.instagram.lite',
+        }
+
+        package_name = packages_map.get(self.automation_app, None)
+        if package_name is None:
+            raise ValueError(f'Invalid automation app: {self.automation_app}')
+        self.device_automator.app_start(package_name, use_monkey=True)
+        return self.device_automator.app_wait(package_name)
+
+    def click(self, element_selectors: dict, timeout: int = 10) -> None:
         element = self.find_element(element_selectors)
         element.click(timeout=timeout)
 
-    def send_keys(self, element_selectors: dict, keys: str) -> None:
+    def send_keys(
+        self, element_selectors: dict, keys: str, timeout: int = 10
+    ) -> None:
         element = self.find_element(element_selectors)
+        self.wait_for_element(element, timeout)
         element.set_text(keys)
 
-    def wait_for_element(self, element_selectors: dict, seconds: int) -> bool:
-        element = self.find_element(element_selectors)
-        return element.wait(timeout=seconds)
+    def wait_for_element(self, element: u2.UiObject, seconds: int) -> None:
+        if not element.wait(timeout=seconds):
+            raise TimeoutError(f'Element not found: {element.info}')
 
-    def find_element(self, selectors: dict) -> u2.UiObject:
+    def find_element(self, selectors: dict) -> u2.UiObject | u2.xpath.XPath:
+
+        if 'xpath' in selectors.keys():
+            element = self.device_automator.xpath(selectors['xpath'])
+            return element
+
         element = self.device_automator(**selectors)
         return element
